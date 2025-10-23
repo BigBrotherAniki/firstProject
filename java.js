a218701adc34e083042fe6f94d2fa5cc21aead15
BigBrotherAniki <ajdin.masic99@gmail.com>
2025-10-23 22:15:21 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,56 @@
+const images = [
+      "asuka_image1.jpg",
+      "misato_image2.jpg",
+      "mari_image3.jpg",
+      "rei_image4.jpg"
+    ];
+
+    const names = [
+      "Asuka",
+      "Misato",
+      "Mari",
+      "Rei"
+    ];
+
+    let index = 0;
+    let smashCount = 0;
+    let passCount = 0;
+
+    // Track individual smashes per character
+    let smashPerImage = new Array(images.length).fill(0);
+
+    // Preload images
+    images.forEach(src => {
+      const img = new Image();
+      img.src = src;
+    });
+
+    function vote(type) {
+      if (type === 'smash') {
+        smashCount++;
+        smashPerImage[index]++;
+      } else if (type === 'pass') {
+        passCount++;
+      }
+
+      // Go to next image
+      index = (index + 1) % images.length;
+      document.getElementById("mainImage").src = images[index];
+      document.getElementById("charName").innerText = names[index];
+
+      updateCounter();
+    }
+
+    function updateCounter() {
+      document.getElementById("counter").innerText = `Smash: ${smashCount} | Pass: ${passCount}`;
+
+      let scoreboardHTML = "<h3>Scoreboard:</h3><ul>";
+      names.forEach((name, i) => {
+        scoreboardHTML += `<li>${name}: ${smashPerImage[i]} Smash(es)</li>`;
+      });
+      scoreboardHTML += "</ul>";
+      document.getElementById("scoreboard").innerHTML = scoreboardHTML;
+    }
+
+    // Initialize scoreboard on page load
+    updateCounter();
